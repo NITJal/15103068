1eee457d075ecf14fffd8ac93db58815bf29c215
vikas-nitj <vikas1000ad@gmail.com>
2017-09-15 20:53:54 +0530

    Add files via upload

---
+++ Hw1/Prime.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/*@15103068
+*/
+
+class Prime
+{
+	public static void main(String[] args)
+	{
+		int n,p;
+
+                // take input   
+		Scanner s=new Scanner(System.in);
+		System.out.println(“Enter the number upto which prime numbers are needed”);
+		n=s.nextInt();
+ 		// print all prime numbers upto n
+		for(int i=2;i<n;i++)
+		{
+			p=0;
+			for(int j=2;j<i;j++)
+			{
+				if(i%j==0)
+				p=1;
+			}
+			if(p==0)
+				System.out.println(i);
+		}
+	}
+}
